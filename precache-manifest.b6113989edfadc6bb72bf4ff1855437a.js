0cd7f24a9295b68c2e68890d72f6195ce2a13510
NIIISH <1nikitashenderov@gmail.com>
2019-04-23 22:32:51 +0300

    Updates

---
+++ precache-manifest.b6113989edfadc6bb72bf4ff1855437a.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "178cff056840a2b3545cec6a26b50394",
+    "url": "/cvBank/static/media/Image 3@2x.178cff05.png"
+  },
+  {
+    "revision": "62994c4a675b88f6dabd",
+    "url": "/cvBank/static/css/main.2a592d57.chunk.css"
+  },
+  {
+    "revision": "c49f503708712462b636",
+    "url": "/cvBank/static/js/runtime~main.47dc1e12.js"
+  },
+  {
+    "revision": "147473d8473b2d65b644",
+    "url": "/cvBank/static/js/2.80c32044.chunk.js"
+  },
+  {
+    "revision": "f95b65daa8c6301d3176ba9398ee784d",
+    "url": "/cvBank/static/media/Group 381.f95b65da.png"
+  },
+  {
+    "revision": "0fe00bcd93ebc7c26e2c21a91756d479",
+    "url": "/cvBank/static/media/Group 381@2x.0fe00bcd.png"
+  },
+  {
+    "revision": "47d02cc092b3cc7f28d96ffa404a92c6",
+    "url": "/cvBank/static/media/Group 431.47d02cc0.png"
+  },
+  {
+    "revision": "363791adb4f0a8e70cbd99bbd0b56a64",
+    "url": "/cvBank/static/media/Group 431@2x.363791ad.png"
+  },
+  {
+    "revision": "01926a3c47d688d852202e670fcbdf60",
+    "url": "/cvBank/static/media/Image 3.01926a3c.png"
+  },
+  {
+    "revision": "62994c4a675b88f6dabd",
+    "url": "/cvBank/static/js/main.6f64377f.chunk.js"
+  },
+  {
+    "revision": "af3e5c2cd5f3a0f7da5e7508fbf983e3",
+    "url": "/cvBank/static/media/Intersection 1@2x.af3e5c2c.png"
+  },
+  {
+    "revision": "34aa1555bb239cded1b6eeb169a66893",
+    "url": "/cvBank/static/media/blur-overlay-png-5.34aa1555.png"
+  },
+  {
+    "revision": "6abfaf5e8ba5dbb29624cfb1e67a792d",
+    "url": "/cvBank/static/media/templateIco_1.6abfaf5e.png"
+  },
+  {
+    "revision": "008972c7d427caaf67c7e9326f663320",
+    "url": "/cvBank/static/media/1511.008972c7.svg"
+  },
+  {
+    "revision": "2205f191b8f5e176861c6b39794eeef4",
+    "url": "/cvBank/static/media/17580.2205f191.svg"
+  },
+  {
+    "revision": "60a34a4a07377c3f22f334ee88875cfd",
+    "url": "/cvBank/static/media/xpng.60a34a4a.png"
+  },
+  {
+    "revision": "9b747117c551c6462f1d4ba1f330b60c",
+    "url": "/cvBank/static/media/bg1.9b747117.svg"
+  },
+  {
+    "revision": "ac5d817f751c68d9ae5b32081fd08a1c",
+    "url": "/cvBank/static/media/bg3.ac5d817f.svg"
+  },
+  {
+    "revision": "b17d863b8eb72533518336bf32be35da",
+    "url": "/cvBank/index.html"
+  }
+];
